7da340d28625fc46cb103d7d6eef4556c581b229
Akshay <akshayjoshi4770@gmail.com>
2022-01-10 19:54:25 +0530

    used advice slip api to generate quotes and added bg image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import axios from "axios";
+import React,{useState,useEffect} from "react";
 import './App.css';
 
+
+
 const App = () => {
+  const  [quote, setQuote] = useState("")
+  async function getQuote() {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      setQuote(response.data.slip.advice);
+      console.log(quote);
+   }
+
     return (
-        <div className='App'>
-            <h1>Random Quotes Generator</h1>
+        <div className="App">
+            <h1 className="title">Random Quote Generator</h1>
+            
+             <div className="quote-wrapper">
+                  <h3> "{quote}" </h3> 
+             </div>
+            <button onClick={getQuote} >GET QUOTE </button>
         </div>
     )
 }
